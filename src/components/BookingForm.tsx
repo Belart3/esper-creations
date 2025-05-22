7c7ddd255034f49549dae120a632a710fdd6f31d
Belat <enochbelawu8@gmail.com>
2025-05-22 22:45:25 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -3,12 +3,13 @@ import { Inter } from "next/font/google";
 import PhoneInput from 'react-phone-number-input';
 import services from "@/data/services.json"
 import { sendBookingMail } from '@/lib/actions/sendMailAction';
+import { toast } from 'react-toastify';
+import { ScaleLoader } from 'react-spinners';
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
-
 
 const BookingForm = () => {
     const [footerForm, setFooterForm] = useState({
@@ -18,13 +19,40 @@ const BookingForm = () => {
         service: '',
         issue: ''
     })
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        console.log(footerForm)
+        try {
+            const res = await fetch('https://esper-backend.onrender.com/send-booking-form', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(footerForm),
+            });
+
+            const data: { message?: string } = await res.json();
+
+            if (!res.ok) throw new Error(data.message || 'Failed to send');
+            setFooterForm({ name: '', email: '', phone: '', issue: '', service: '' });
+            toast.success("Email sent successfully!");
+        } catch (err) {
+            console.error(err);
+            toast.error("Failed to send email.");
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
   return (
-    <form action={sendBookingMail} className="flex flex-col gap-4 md:gap-6 w-full z-10">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 md:gap-6 w-full z-10">
         <input 
         name="name"
         aria-label="name"
         type="text" 
-        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300 ${ footerForm.name.trim() !== '' ? '!bg-[#FFEEE6]' : '' }`} 
+        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#666666] p-4 rounded-[16px] placeholder-shown:text-[#666666] placeholder-shown:text-[14px]/[21px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300 ${ footerForm.name.trim() !== '' ? '!bg-[#FFEEE6]' : '' }`} 
         placeholder="Name *" 
         required 
         value={footerForm.name}
@@ -36,7 +64,7 @@ const BookingForm = () => {
         name="email"
         aria-label="email"
         type="email" 
-        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300 ${ footerForm.email.trim() !== '' && footerForm.email.includes('@') ? '!bg-[#FFEEE6]' : '' }`} 
+        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#666666] p-4 rounded-[16px] placeholder-shown:text-[#666666] placeholder-shown:text-[14px]/[21px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300 ${ footerForm.email.trim() !== '' && footerForm.email.includes('@') ? '!bg-[#FFEEE6]' : '' }`} 
         placeholder="Email *" 
         required 
         value={footerForm.email}
@@ -46,6 +74,7 @@ const BookingForm = () => {
         />
         <PhoneInput
         name="phone"
+        required
         aria-label="phone"
         placeholder="Phone Number"
         defaultCountry="US"
@@ -53,13 +82,13 @@ const BookingForm = () => {
         onChange={(value) => {
             setFooterForm({ ...footerForm, phone: value || '' });
         }}
-        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300 ${ footerForm.phone.trim() !== '' ? '!bg-[#FFEEE6]' : '' }`}
+        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#666666] p-4 rounded-[16px] placeholder-shown:text-[#666666] placeholder-shown:text-[14px]/[21px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300 ${ footerForm.phone.trim() !== '' ? '!bg-[#FFEEE6]' : '' }`}
         />
         <select 
         defaultValue='What service are you in need of? *'
         name="service" 
-        id="" 
-        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300`}
+        required
+        className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#666666] p-4 rounded-[16px] bg-[#E6E6E699] focus:outline-[#FF5600] focus:border-[#FF5600] focus:ring-[#FF5600] border-none outline-[#FF5600] transition-all ease-linear duration-300`}
         value={footerForm.service}
         onChange={
             (e) => {
@@ -68,12 +97,12 @@ const BookingForm = () => {
             }
         }
         >
-            <option value="A brief description of your issue (Optional)" disabled className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699]`}>
+            <option value="" selected hidden disabled className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699]`}>
                 What service are you in need of? *
             </option>
         {
         services.map((service, index) => (
-            <option value={service.title} className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#B3B3B3] p-4 rounded-[16px] bg-[#E6E6E699]`} key={index}>
+            <option value={service.title} className={`${inter.className} text-[14px]/[21px] tracking-[-0.56px] text-[#666666] p-4 rounded-[16px] bg-[#E6E6E699]`} key={index}>
             {service.title}
             </option>
         ))
@@ -83,7 +112,6 @@ const BookingForm = () => {
         aria-label="issue"
         value={footerForm.issue}
         name="issue" 
-        id="" 
         onChange={(e) => {
             setFooterForm({
             ...footerForm,
@@ -94,10 +122,17 @@ const BookingForm = () => {
         placeholder="A brief description of your issue (Optional)"
         ></textarea>
         <button 
-        className={`${inter.className} rounded-[32px] bg-[#FF5600] py-5 px-6 capitalize text-white font-semibold text-[16px]/[16px] tracking-[-0.64px] w-full lg:w-fit custom-shadow-orange cursor-pointer mx-auto transition-all ease-linear duration-300 hover:!shadow-none`}
-        type="submit"
+        className={`${inter.className} rounded-[32px] bg-[#FF5600] py-5 px-6 capitalize text-white font-semibold text-[16px]/[16px] tracking-[-0.64px] w-full lg:w-fit custom-shadow-orange cursor-pointer transition-all ease-linear duration-300 hover:!shadow-none max-w-[300px] mx-auto`}
+        type='submit'
         >
-            transform my online presence!
+            {
+            isSubmitting ? (
+                <div className="flex items-center justify-center gap-1">
+                    Submitting
+                    <ScaleLoader height={10} color='white' />
+                </div>
+            ) : 'Transform my online presence!'
+            }
         </button>
     </form>
   )
--- src/components/HeroForm.tsx
+++ src/components/HeroForm.tsx
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google';
 import React from 'react'
 import { useState } from 'react'
 import PhoneInput from 'react-phone-number-input';
+import { ScaleLoader } from 'react-spinners';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const inter = Inter({
@@ -12,14 +15,41 @@ const inter = Inter({
 
 
 const HeroForm = () => {
-    // hero form fields logic
+    const [isSubmitting, setIsSubmitting] = useState(false);
     const [heroForm, setHeroForm ] = useState({
         fullName: '',
         email: '',
         phone: ''
-    })
+    });
+
+    interface SubmitEvent extends React.FormEvent<HTMLFormElement> {}
+
+    const handleSubmit = async (e: SubmitEvent) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        console.log(heroForm)
+        try {
+            const res = await fetch('https://esper-backend.onrender.com/send-hero-form', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(heroForm),
+            });
+
+            const data: { message?: string } = await res.json();
+
+            if (!res.ok) throw new Error(data.message || 'Failed to send');
+            setHeroForm({ fullName: '', email: '', phone: '' });
+            toast.success("Email sent successfully!");
+        } catch (err) {
+            console.error(err);
+            toast.error("Failed to send email.");
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
 return (
-    <form action={sendHeroFormEmail} className="flex flex-col xl:flex-row gap-3 xl:gap-6 w-full xl:w-fit">
+    <form onSubmit={handleSubmit} className="flex flex-col xl:flex-row gap-3 xl:gap-6 w-full xl:w-fit">
         <input
         name="fullName"
         aria-label="full name" 
@@ -61,8 +91,16 @@ return (
         className={`${inter.className} rounded-[32px] bg-[#FF5600] py-5 px-6 capitalize text-white font-semibold text-[16px]/[16px] tracking-[-0.64px] w-full lg:w-fit custom-shadow-orange cursor-pointer transition-all ease-linear duration-300 hover:!shadow-none mx-auto`}
         type='submit'
         >
-        transform my online presence!
+            {
+            isSubmitting ? (
+                <div className="flex items-center justify-center gap-1">
+                    Submitting
+                    <ScaleLoader height={10} color='white' />
+                </div>
+            ) : 'Transform my online presence!'
+            }
         </button>
+        <ToastContainer />
     </form>
   )
 }
